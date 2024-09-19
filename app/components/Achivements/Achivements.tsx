060799e58bc971ad310a069e26b9fb972668a633
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-19 15:18:42 +0400

    .

--- app/components/Achivements/Achivements.tsx
+++ app/components/Achivements/Achivements.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image'; // Import Image from Next.js for optimized images
 import styles from './Achivements.module.css'; // Import CSS module
+import kbili1 from "../public/assets/kbili1.jpg"
+import kbili2 from "../public/assets/kbili2.jpg"
+import kbili3 from "../public/assets/kbili3.jpg"
+import kbili4 from "../public/assets/kbili4.jpg"
 
 const Achievements = () => {
   return (
@@ -7,10 +11,10 @@ const Achievements = () => {
       <center>
         <div className={styles.bigBox}>
           <h2 className={styles.h2}>ჩვენი მიღწევები</h2>
-          <Image src="/assets/kbili1.jpg" alt="Achievement 1" height={300} width={300} className={styles.image} />
-          <Image src="/assets/kbili2.jpg" alt="Achievement 2" height={300} width={300} className={styles.image} />
-          <Image src="/assets/kbili3.jpg" alt="Achievement 3" height={300} width={300} className={styles.image} />
-          <Image src="/assets/kbili4.jpg" alt="Achievement 4" height={300} width={300} className={styles.image} />
+          <Image src={kbili1} alt="Achievement 1" height={300} width={300} className={styles.image} />
+          <Image src={kbili2} alt="Achievement 2" height={300} width={300} className={styles.image} />
+          <Image src={kbili3} alt="Achievement 3" height={300} width={300} className={styles.image} />
+          <Image src={kbili4} alt="Achievement 4" height={300} width={300} className={styles.image} />
         </div>
       </center>
     </div>
--- app/components/DentalCard/DentalCard.tsx
+++ app/components/DentalCard/DentalCard.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './DentalCard.module.css';
 import Image from 'next/image';
-import kbili from "../public/assets/kbili1.jpg"
+import kbili from "../public/assets/noimage.png"
 
 const DentalCard = ({ nivti }) => {
   return (
     <div className={styles.boX}>
       <div className={styles.dentalCard}>
-        <Image src="/../public/assets/kbili1.jpg" alt="errors" width={300} height={300} className={styles.cardImg} />
+        <Image src={kbili} alt="errors" width={289} height={200} className={styles.cardImg} />
         <h1 className={styles.h1}>{nivti.name}</h1>
         <h2 className={styles.h2}>{nivti.profession}</h2>
       </div>
