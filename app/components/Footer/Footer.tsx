d66e69273e4bed07b05b65b8371fd12b55bd1296
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-19 22:14:25 +0400

    Update Footer.tsx

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -5,26 +5,29 @@ import logo from "../../public/assets/logo2.jpg"
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <center>
-        <Image
-          src={logo}
-          alt="error"
-          className={styles.footerImg}
-          height={150}
-          width={300}
-        />
-      </center>
-      <center>
-        <h2 className={styles.footerText}>📞 574 42 42 38</h2>
-      </center>
-      <center>
-        <hr className={styles.footerHr} />
-      </center>
-      <center>
-        <h2 className={styles.footerText}>🗺️ პეკინის ქუჩა 17</h2>
-      </center>
-    </div>
+    <>
+      <div className={styles.footer}>
+        <center>
+          <Image
+            src={logo}
+            alt="error"
+            className={styles.footerImg}
+            height={150}
+            width={300}
+          />
+        </center>
+        <center>
+          <h2 className={styles.footerText}>📞 574 42 42 38</h2>
+        </center>
+        <center>
+          <hr className={styles.footerHr} />
+        </center>
+        <center>
+          <h2 className={styles.footerText}>🗺️ პეკინის ქუჩა 17</h2>
+        </center>
+      </div>
+      <br /><br /><br />
+    </>
   );
 };
 
