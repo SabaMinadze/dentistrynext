1b33a813ed2c6a9262db95ac50e2784b3c3df050
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-19 18:35:52 +0400

    .

--- app/components/Head3/Head3.tsx
+++ app/components/Head3/Head3.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from './Head3.module.css';
-import logo from '../../public/assets/logo2.jpg';
+import styles from './Head3.module.css'; // Import your CSS module
+import logo from '../../public/assets/logo2.jpg'; // Import your image
 
 const Head3: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.h1}>დენტისტრი</h1>
-      <button className={styles.button}>კითხვა გაქვთ? დაგვიკავშირდით</button>
-      <Image src={logo} alt="Logo" className={styles.logo} />
+    <div>
+      <div className={styles.box}>
+        <center><h1 className={styles.h1}>დენტისტრი</h1></center>
+        <center><button className={styles.button}>კითხვა გაქვთ? დაგვიკავშირდით</button></center>
+        <center><Image src={logo} alt="Logo" className={styles.logo} /></center>
+      </div>
     </div>
   );
 };
 
 export default Head3;
-
