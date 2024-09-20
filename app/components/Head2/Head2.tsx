0618e5c0080dddab3cdffd76a42ee2154db4a1e9
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-20 13:51:33 +0400

    Update Head2.tsx

--- app/components/Head2/Head2.tsx
+++ app/components/Head2/Head2.tsx
@@ -11,10 +11,10 @@ const Head2 = () => {
         <Link href="/"><Image src={logo} alt="error" height={110} width={190} className={styles.logo} /></Link>
         <div className={styles.vl}><h4 className={styles.txt}>სტომატოლოგიური სასწავლო ცენტრი დენტისტრი</h4></div>
         <ul className={styles.navList}>
-          <li><p className={styles.p}>მთავარი</p></li>
+          <Link href="/"><li><p className={styles.p}>მთავარი</p></li></Link>
           <Link href="/servicesandprices"><li><p className={styles.p}>სერვისები და ფასები</p></li></Link>
           <li><p className={styles.p}>რეზიდენტურა</p></li>
-          <Link href="/dentists"><li><p className={styles.p}>ჩვენი სტომატოლოგები</p></li></Link>
+          <Link href="/dentists"><li><p className={styles.p}>ჩვენი გუნდი</p></li></Link>
           <li><p className={styles.p}>ჩვენს შესახებ</p></li>
           <li><p className={styles.p}>კონტაქტი</p></li>
         </ul>
